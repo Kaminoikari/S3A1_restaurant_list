285500a990d7ee46a699056d02259c48f6a83a6d
Charles <charles0527@hotmail.com>
2022-03-07 01:01:56 +0800

    feat: add CRUD function via Mongoose

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 "use strict";
 const express = require("express");
 const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser')
 const restaurantList = require("./restaurant.json");
 const app = express();
 const port = 3000;
+
+// Set up connection w/ mongoDB database
+mongoose.connect ("mongodb://localhost/restaurant_list")
+const db = mongoose.connect; // 取得資料庫連彥狀態
+
+// 連線異常/失敗
+db.on("error", () => {
+  console.log("mongoDB error!!")
+})
+
+// 連線成功
+db.once("open", () => {
+  console.log("mongoDB connected!!")
+})
+
+// 使用app.use規定每筆請球都需先透過body-parser處理
+app.use (bodyParser.urlencoded, ({ extended: true }))
 
 // express template engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -14,8 +33,55 @@ app.use(express.static("public"));
 
 // render index page
 app.get("/", (req, res) => {
-  res.render("index", { restaurants: restaurantList.results });
+  restaurantList
+  .find ()
+  .lean ()
+  .then ((restaurants) => res.render ("index", { restaurants }))
+  .catch((error) => console.log (error))
 });
+
+// render edit page
+app.get("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id
+  .findById (id)
+  .lean ()
+  .then ((restaurant) => res.render ("edit", { restaurant }))
+  .catch((error) => console.log (error))
+});
+
+// save edit page
+app.post("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id
+  const newRestaurant = req.body
+  return restaurantList
+  .findOneAndUpdate ({ _id:id }, newRestaurant)
+  .then (res.redirect ('/'))
+  .catch((error) => console.log(error))
+});
+
+// delete restaurant
+app.post("/restaurants/:id/delete", (req, res) => {
+  const id = req.params.id
+  restaurantList
+  .findById (id)
+  .then((restaurant) => restaurant.delete())
+  .then (res.redirect ('/'))
+  .catch((error) => console.log(error))
+});
+
+// render create page
+app.get ("/restaurants/create", (req, res) => {
+  res.render ("create")
+})
+
+// submit create page
+app.post ("/restaurants/new", (req, res) => {
+  const restaurant = req.body
+  return restaurantList
+  .create (restaurant)
+  .then(() => res.redirect ('/'))
+  .catch((error) => console.log(error))
+})
 
 // render show page
 app.get("/restaurants/:restaurants_id", (req, res) => {
@@ -29,7 +95,7 @@ app.get("/restaurants/:restaurants_id", (req, res) => {
 
 // render search page
 app.get("/search", (req, res) => {
-  console.log(req.query.keyword);
+  
   const restaurants = restaurantList.results.filter(
     (restaurant) =>
       restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase()) ||
