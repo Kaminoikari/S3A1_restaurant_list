6ff277dad0b6b9f2ad88b4dd44fc62944bcaeca1
Charles <charles0527@hotmail.com>
2022-03-10 17:08:13 +0800

    feat: add CRUD function

--- app.js
+++ app.js
@@ -7,22 +7,21 @@ const restaurantList = require("./restaurant.json");
 const app = express();
 const port = 3000;
 
-// Set up connection w/ mongoDB database
-mongoose.connect ("mongodb://localhost/restaurant_list")
-const db = mongoose.connect; // 取得資料庫連彥狀態
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
-// 連線異常/失敗
-db.on("error", () => {
-  console.log("mongoDB error!!")
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
 })
-
 // 連線成功
-db.once("open", () => {
-  console.log("mongoDB connected!!")
+db.once('open', () => {
+  console.log('mongodb connected!')
 })
 
-// 使用app.use規定每筆請球都需先透過body-parser處理
-app.use (bodyParser.urlencoded, ({ extended: true }))
+// 使用app.use規定每筆請求都需先透過body-parser處理
+app.use( bodyParser.urlencoded({ extended: true }) )
 
 // express template engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
