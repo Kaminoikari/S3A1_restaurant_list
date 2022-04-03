1e0bf9a7c01652a9a5659502a29104ab44cc93dc
Charles <charles0527@hotmail.com>
2022-04-03 18:05:14 +0800

    fix: user modules

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
+const bodyParser = require('body-parser');
 const methodOverride = require ('method-override')
-const mongoose = require("mongoose")
-const bodyParser = require('body-parser')
-const restaurantList = require("./models/restaurant")
-require('./config/mongoose.js')
+const session = require ('express-session')
+const flash = require ('connect-flash')
 
+if (process.env.NODE_ENV !== 'production') {
+  require ('dotenv').config()
+}
+
+const mongoose = require("mongoose")
+const restaurantList = require("./models/restaurant")
 const app = express()
 const port = 3000
+
 const routes = require('./routes')
+const { config } = require ('dotenv')
+require('./config/mongoose.js');
 
 // express template engine
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: '.hbs'}))
@@ -16,6 +24,12 @@ app.set("view engine", 'hbs')
 
 // setting static files
 app.use(express.static("public"))
+
+app.use (session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+}))
 
 // 使用app.use規定每筆請求都需先透過body-parser處理
 app.use( bodyParser.urlencoded({ extended: true }) )
---
+++ routes/modules/users.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/user');
+
+// Login
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.post(
+  '/login',
+  (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    if (!email || !password) {
+      req.flash('warning_msg', 'Email or Password is empty');
+    }
+    next();
+  },
+  passport.authenticate('local', {
+    failureRedirect: '/users/login',
+    successRedirect: '/',
+  })
+);
+
+// Register
+router.get('/register', (req, res) => {
+  res.render('register');
+});
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+  const errors = [];
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' });
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' });
+  }
+
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword,
+    });
+  }
+
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        errors.push({
+          message: '這個Email已經註冊過了。',
+        });
+        return res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          confirmPassword,
+        });
+      }
+
+      return bcrypt
+        .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+        .then((salt) => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+        .then((hash) =>
+          User.create({
+            name,
+            email,
+            password: hash, // 用雜湊值取得原本的使用者密碼
+          })
+        )
+        .then(() => res.redirect('/'))
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', '您已經成功登出。');
+  res.redirect('/users/login');
+});
+
+module.exports = router;
