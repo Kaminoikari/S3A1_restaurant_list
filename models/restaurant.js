dcbbc9da1e905ca6e01b957814ffa96eec484f56
Charles <charles0527@hotmail.com>
2022-04-06 03:07:56 +0800

    fix: name is not required for registration

--- models/restaurant.js
+++ models/restaurant.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
   name: { 
     type: String, 
-    required: true 
+    required: false
   },
   name_en: { 
     type: String, 
